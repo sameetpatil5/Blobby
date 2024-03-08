d956f66ad3870752a48e8eae86307590d84a4d88
Sameet Patil <sameetpatil5@gmail.com>
2024-03-08 22:47:48 +0530

    Add Contact functionality in main.py

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+import smtplib
 
-# Optional: add contact me email functionality (Day 60)
-# import smtplib
 load_dotenv()
+
+MAIL_ADDRESS = os.environ.get("EMAIL")
+MAIL_APP_PW = os.environ.get("PASSWORD")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
@@ -272,31 +274,28 @@ def about():
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html", current_user=current_user)
-
-
-# Optional: You can include the email sending code from Day 60:
-# DON'T put your email and password here directly! The code will be visible when you upload to Github.
-# Use environment variables instead (Day 35)
-
-# MAIL_ADDRESS = os.environ.get("EMAIL_KEY")
-# MAIL_APP_PW = os.environ.get("PASSWORD_KEY")
-
-# @app.route("/contact", methods=["GET", "POST"])
-# def contact():
-#     if request.method == "POST":
-#         data = request.form
-#         send_email(data["name"], data["email"], data["phone"], data["message"])
-#         return render_template("contact.html", msg_sent=True)
-#     return render_template("contact.html", msg_sent=False)
-#
-#
-# def send_email(name, email, phone, message):
-#     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
-#     with smtplib.SMTP("smtp.gmail.com") as connection:
-#         connection.starttls()
-#         connection.login(MAIL_ADDRESS, MAIL_APP_PW)
-#         connection.sendmail(MAIL_ADDRESS, MAIL_APP_PW, email_message)
+    if request.method == "POST":
+        MESSAGE = f"Subject: {request.form['name']} has sent a messsage!!!\n\n \
+                    Name: {request.form['name']}\n \
+                    E-mail: {request.form['email']}\n \
+                    Phone No.: {request.form['phone']}\n \
+                    Message: {request.form['message']}"
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MAIL_ADDRESS, password=MAIL_APP_PW)
+            connection.sendmail(
+                from_addr=MAIL_ADDRESS, to_addrs=MAIL_ADDRESS, msg=MESSAGE
+            )
+            connection.close()
+        return render_template("contact.html", msg_sent=True)
+    else:
+        if current_user.is_authenticated:
+            name = current_user.name
+            email = current_user.email
+        else:
+            name = ""
+            email = ""
+        return render_template("contact.html", msg_sent=False, name=name, email=email)
 
 
 if __name__ == "__main__":
