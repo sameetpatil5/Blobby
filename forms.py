bec1a5c8da92c46c019e71166d2db03d590f711b
Sameet Patil <sameetpatil5@gmail.com>
2024-03-09 11:05:04 +0530

    Add comments for documentation

--- forms.py
+++ forms.py
@@ -6,8 +6,18 @@ from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
 
 
-# WTForm for creating a Blog Post
-class CreatePostForm(FlaskForm):
+# WTForm for creating & editing a Blog Post
+class BlogPostForm(FlaskForm):
+    """
+    CreatePostForm is used to create a new blog post or edit an existing blog post.
+    It contains the following fields:
+    - title: A required string field for the blog post title.
+    - subtitle: A required string field for the blog post subtitle.
+    - img_url: A required URL field for the blog post image URL.
+    - body: A required CKEditor field for the blog content.
+    - submit: A submit field for submitting the form.
+    """
+
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     img_url = URLField("Blog Image URL", validators=[DataRequired(), URL()])
@@ -17,6 +27,15 @@ class CreatePostForm(FlaskForm):
 
 # WTForm to Register new users
 class RegisterForm(FlaskForm):
+    """
+    RegisterForm is used for registering new users.
+    It contains the following fields:
+    - email: A required email field for the user's email address.
+    - password: A required password field for the user's password.
+    - name: A required string field for the user's name.
+    - submit: A submit field for submitting the form.
+    """
+
     email = EmailField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     name = StringField("Name", validators=[DataRequired()])
@@ -25,6 +44,14 @@ class RegisterForm(FlaskForm):
 
 # WTForm to Login existing users
 class LoginForm(FlaskForm):
+    """
+    LoginForm is used for users to log in.
+    It contains the following fields:
+    - email: A required email field for the user's email address.
+    - password: A required password field for the user's password.
+    - submit: A submit field for submitting the form.
+    """
+
     email = EmailField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Log In")
@@ -32,12 +59,28 @@ class LoginForm(FlaskForm):
 
 # WTForm to add comments under Blog Post
 class CommentForm(FlaskForm):
+    """
+    CommentForm is used for users to add comments under a blog post.
+    It contains the following fields:
+    - comment_text: A required CKEditor field for the user's comment.
+    - submit: A submit field for submitting the form.
+    """
+
     comment_text = CKEditorField("Comment", validators=[DataRequired()])
     submit = SubmitField("Submit Comment")
 
 
 # WTForm to contact the Admin
 class ContactFrom(FlaskForm):
+    """
+    ContactForm is used for users to contact the admin.
+    It contains the following fields:
+    - name: A required string field for the user's name
+    - email: A required email field for the user's email address.
+    - message: A required string field for the user's message.
+    - submit: A submit field for submitting the form.
+    """
+
     name = StringField("Name", validators=[DataRequired()])
     email = EmailField("Email Address", validators=[DataRequired(), Email()])
     message = StringField("Message", validators=[DataRequired()])
