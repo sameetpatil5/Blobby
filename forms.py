6b8cab148dbc09f2e144194c3d4bd4617857efd0
Sameet Patil <sameetpatil5@gmail.com>
2024-03-09 10:57:02 +0530

    Add new ContactFrom to forms.py

--- forms.py
+++ forms.py
@@ -1,34 +1,44 @@
+# forms.py - This file contains various WTForms used in the application.
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL
+from wtforms import EmailField, StringField, SubmitField, PasswordField, URLField
+from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
 
 
-# WTForm for creating a blog post
+# WTForm for creating a Blog Post
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    img_url = URLField("Blog Image URL", validators=[DataRequired(), URL()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
 
-# Create a form to register new users
+# WTForm to Register new users
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     name = StringField("Name", validators=[DataRequired()])
-    submit = SubmitField("Sign Me Up!")
+    submit = SubmitField("Register")
 
 
-# Create a form to login existing users
+# WTForm to Login existing users
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Let Me In!")
+    submit = SubmitField("Log In")
 
 
-# Create a form to add comments
+# WTForm to add comments under Blog Post
 class CommentForm(FlaskForm):
     comment_text = CKEditorField("Comment", validators=[DataRequired()])
     submit = SubmitField("Submit Comment")
+
+
+# WTForm to contact the Admin
+class ContactFrom(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = EmailField("Email Address", validators=[DataRequired(), Email()])
+    message = StringField("Message", validators=[DataRequired()])
+    submit = SubmitField("Send Message")
